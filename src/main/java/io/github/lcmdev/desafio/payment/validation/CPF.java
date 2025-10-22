5894547378ac067d93b2ab9e7e574b487cb8a893
luciano-mota <amoosenhorjesus3@gmail.com>
2025-10-22 12:19:25 -0300

    criado anotação personalizada para validação de CPF

---
+++ src/main/java/io/github/lcmdev/desafio/payment/validation/CPF.java
@@ -0,0 +1,16 @@
+package io.github.lcmdev.desafio.payment.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CPFValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CPF {
+
+    String message() default "CPF inválido";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
