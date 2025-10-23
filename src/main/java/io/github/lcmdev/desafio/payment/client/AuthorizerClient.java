5e18f51073d8bc339426a9bd19be58c4eeb8489a
luciano-mota <amoosenhorjesus3@gmail.com>
2025-10-23 10:39:23 -0300

    Criado client para chamar rota externa para autorização de pagamento.

---
+++ src/main/java/io/github/lcmdev/desafio/payment/client/AuthorizerClient.java
@@ -0,0 +1,34 @@
+package io.github.lcmdev.desafio.payment.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+import static java.util.Objects.nonNull;
+
+@Component
+public class AuthorizerClient {
+
+    private final WebClient webClient;
+
+    public AuthorizerClient(@Value("${external.authorizer.payment.url}") String baseUrl) {
+        this.webClient = WebClient.builder()
+            .baseUrl(baseUrl)
+            .build();
+    }
+
+    public boolean authorize() {
+        try {
+            String response = webClient.get()
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .timeout(Duration.ofSeconds(5))
+                    .block();
+            return nonNull(response) && (response.toLowerCase().contains("authorized") || response.toLowerCase().contains("approved"));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
