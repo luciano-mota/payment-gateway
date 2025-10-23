10d4521dd1eb11b3adcf105e050103d475fcafca
luciano-mota <amoosenhorjesus3@gmail.com>
2025-10-23 19:51:50 -0300

    Adicionado anotações do lombok na entidade e ajuste nas importações.

--- src/main/java/io/github/lcmdev/desafio/payment/model/Account.java
+++ src/main/java/io/github/lcmdev/desafio/payment/model/Account.java
@@ -1,12 +1,22 @@
 package io.github.lcmdev.desafio.payment.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_accounts")
 public class Account {
