8d5a3c603ceffc8d76788ffdf3f5db4b615d1595
luciano-mota <amoosenhorjesus3@gmail.com>
2025-10-23 12:56:42 -0300

    Remoção de classe que não será mais usada

--- src/main/java/io/github/lcmdev/desafio/payment/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.lcmdev.desafio.payment.repository;
-
-import io.github.lcmdev.desafio.payment.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
